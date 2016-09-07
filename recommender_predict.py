4727c2236615e5f741afe6031499fcc3d422b7ab
Mobile <mobile@Mobiles-MacBook.local>
2016-09-07 16:30:26 -0400

    bug fixes and general improvements

--- recommender_predict.py
+++ recommender_predict.py
@@ -42,36 +42,11 @@ def find_closest_pearson(treatment_correlations, x):
                 highestCorrelationKey = treatment2_key
     return str(highestCorrelationValue) + ":" + highestCorrelationKey
 
-#just like above, except in this case the lowest value is the closest distance
-def find_closest_cosine(treatment_correlations, x):
-    highestCorrelationValue = 3 #highest possible distance is 2
-    highestCorrelationRecommendedTreatment = ""
-    highestCorrelationUserTreatment = ""
-    for treatment1 in x:
-        treatment_correlations.dropna(how='all', axis=1)  #some treatments won't have any associated distances, just skip over them
-        if treatment1 in treatment_correlations:
-            treatment2_index = treatment_correlations[treatment1].idxmin(axis=1)
-            treatment2_value = treatment_correlations[treatment1][treatment2_index]
-            treatment2_key = treatment_correlations.iloc[treatment2_index]['row']
-            if treatment2_value < highestCorrelationValue:
-                highestCorrelationValue = treatment2_value
-                highestCorrelationRecommendedTreatment = treatment2_key
-                highestCorrelationUserTreatment = treatment1
-    return str(highestCorrelationValue) + ":" + highestCorrelationRecommendedTreatment + ":" + highestCorrelationUserTreatment
-
-#Need to send two values back from my transform, so bundling them and then splittng them afterwards
-#TODO gotta be a better way...
-def unpack_value(x):
-    return x.split(":")[0]
-def unpack_recommended_treatment(x):
-    return x.split(":")[1]
-def unpack_user_treatment(x):
-    return x.split(":")[2]
 #get a list of all the conditions this user has
 #we will search each of them to see which one is most actionable
+#I'm just taking the highest and lowest predicted effectiveness for all conditions, could just as easily return a recommendation per condition
 conditions = list(set(test_df['condition']))
 
-#I'm just taking the highest and lowest predicted effectiveness for all conditions, could just as easily do this per condition
 highestPredictedValue = 0 
 highestPredictedKey = ""
 highestPredictedCondition = ""
@@ -82,29 +57,48 @@ for condition in conditions:
     condition_rows = test_df[test_df['condition'] == condition]
     correlations = pd.read_csv(modeldir + '/' + condition.replace('/', '').replace("\n","").replace("\r","") + "_" + distance_metric + ".csv")
 
-    if (distance_metric == 'pearson'):
-        find_closest_func = functools.partial(find_closest_pearson,correlations) #this is so I can pass an argument into my transform function
-    else:
-        find_closest_func = functools.partial(find_closest_cosine,correlations)
-    condition_rows['closest_correlation'] = condition_rows.groupby('user_id')['treatment'].transform(find_closest_func).values
+    # some treatments won't have any associated distances, just skip over them
+    #correlations = correlations.dropna(how='all',axis=1)
 
-    condition_rows['recommended_treatment_value'] = condition_rows['closest_correlation'].apply(unpack_value)
-    condition_rows['recommended_treatment_name'] = condition_rows['closest_correlation'].apply(unpack_recommended_treatment)
-    condition_rows['recommended_user_treatment'] = condition_rows['closest_correlation'].apply(unpack_user_treatment)
-    print condition_rows
+    #looking for the max values for pearson (from -1 to 1 with 1 being closest)
+    #but looking for min values for cosine (from 0-2 with 0 being closest)
+    best_fit_predicted_effectiveness = 0
+    best_fit_treatment_name = ""
+    if (distance_metric == 'pearson'):
+        condition_rows['closest_correlation_name'] = condition_rows['treatment'].apply(
+            lambda x: correlations[correlations['row'] == x].drop('row', axis=1).idxmax(axis=1).values[0])
+        condition_rows['closest_correlation_value'] = condition_rows['treatment'].apply(
+            lambda x: correlations[correlations['row'] == x].drop('row', axis=1).max(axis=1).values[0])
+        condition_rows = condition_rows[pd.notnull(condition_rows['closest_correlation_value'])]
+        if len(condition_rows) > 0:
+            tiedRows = condition_rows[
+                condition_rows['closest_correlation_value'] == condition_rows['closest_correlation_value'].max()]
+    else:
+        condition_rows['closest_correlation_name'] = condition_rows['treatment'].apply(
+            lambda x: correlations[correlations['row'] == x].drop('row', axis=1).idxmin(axis=1).values[0])
+        condition_rows['closest_correlation_value'] = condition_rows['treatment'].apply(
+            lambda x: correlations[correlations['row'] == x].drop('row', axis=1).min(axis=1).values[0])
+        condition_rows = condition_rows[pd.notnull(condition_rows['closest_correlation_value'])]
+        if len(condition_rows) > 0:
+            tiedRows = condition_rows[condition_rows['closest_correlation_value'] == condition_rows['closest_correlation_value'].min()]
+            #in the case of a tie, go with the most relevant predicted effectiveness
+            best_fit_predicted_effectiveness = condition_rows.ix[tiedRows['effectiveness'].abs().idxmax()]['effectiveness']
+            best_fit_treatment_name = condition_rows.ix[tiedRows['effectiveness'].abs().idxmax()]['closest_correlation_name']
+
     #Now we know which treatment that the user has tried has another treatment which is most highly correlated to it, so predict that
     #the new treatment will have an effectiveness similar to the original treatment
-    predicted_value = condition_rows[condition_rows['treatment'] == condition_rows['recommended_user_treatment']]['effectiveness'].values
-    if highestPredictedValue < predicted_value:
-        highestPredictedValue = predicted_value
-        highestPredictedKey = condition_rows['recommended_treatment_name']
+    #print best_fit_predicted_effectiveness
+    #print best_fit_treatment_name
+    if highestPredictedValue < best_fit_predicted_effectiveness:
+        highestPredictedValue = best_fit_predicted_effectiveness
+        highestPredictedName = best_fit_treatment_name
         highestPredictedCondition = condition
-    if lowestPredictedValue > predicted_value:
-        lowestPredictedValue = predicted_value
-        lowestPredictedKey = condition_rows['recommended_treatment_name']
+    if lowestPredictedValue > best_fit_predicted_effectiveness:
+        lowestPredictedValue = best_fit_predicted_effectiveness
+        lowestPredictedName = best_fit_treatment_name
         lowestPredictedCondition = condition
 
 if highestPredictedValue > 0:
-    print "This user may have good results treating " + highestPredictedCondition + " with " + highestPredictedKey
+    print "This user may have good results treating " + highestPredictedCondition + " with " + highestPredictedName
 if lowestPredictedValue < 0:
-    print "This user may have good results treating " + lowestPredictedCondition + " by staying away from " + lowestPredictedKey
+    print "This user may have good results treating " + lowestPredictedCondition + " by staying away from " + lowestPredictedName
