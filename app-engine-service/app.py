5046bd317ab97a074d36af8acfcec5318ae44175
Graydyn <graydyn.young@gmail.com>
2016-11-18 13:57:24 -0500

    first draft up on app engine

--- app-engine-service/app.py
+++ app-engine-service/app.py
@@ -3,55 +3,62 @@ from flask.ext.restful import reqparse, abort, Api, Resource, fields,marshal_wit
 from flask_restful_swagger import swagger
 import csv
 import json
+from google.cloud import datastore
 
 app = Flask(__name__, static_folder='../static')
 
-###################################
-# This is important:
 api = swagger.docs(Api(app), apiVersion='0.1',
                    basePath='http://localhost:5000',
                    resourcePath='/',
                    produces=["application/json", "text/html"],
                    api_spec_url='/api/spec',
                    description='Supplies summary statistics for Flaredown data')
-###################################
 
-def getSymptomsByCondition(condition):
-    condition_dict = {}
-    with open('Symptom_count_by_condition.csv', 'rt') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            print row['Condition']
-            if row['Condition'].lower() == condition.lower():
-                condition_dict = row
-    condition_dict.pop('', None)
-    condition_dict.pop('Condition', None)
-    return condition_dict
+def create_client():
+    project_id = 'flaredown-149515'
+    return datastore.Client(project_id)
 
-def getSymptomTotals():
-    totals = {}
-    infile = open('Symptom_counts.csv')
-    for line in infile:
-        symptom,count = line.split(',')
-        totals[symptom] = count
-    return totals
+def getCountsByCondition(client, trackable_type, condition):
+    query = client.query(kind='Condition' + trackable_type)
+    query.add_filter('condition','=',condition)
+    for result in query.fetch(limit=1):
+        return result
+
+def getTotal(client, trackable_type, trackable_name):
+    query = client.query(kind=trackable_type + 'Count')
+    query.add_filter('name','=',trackable_name)
+    for result in query.fetch(limit=1):
+        return result['count']
+
+def getAllTotals(client, trackable_type):
+    query = client.query(kind=trackable_type + 'Count')
+    totals_dict = {}
+    for result in query.fetch():
+        if result['name'] != 'condition':
+            totals_dict[result['name']] = result['count']
+    return totals_dict
+
+def getConditionList(client):
+    query = client.query(kind='ConditionList')
+    conditions = []
+    for result in query.fetch():
+        conditions.append(result['name'])
+    return conditions
 
 class ConditionsList(Resource):
   @swagger.operation(
-      notes='Returns a comma seperated list of all conditions.',
+      notes='Returns a comma seperated list of all conditions.  Rather than a list of all conditions that have ever been reported, this is a currated list where synonymous conditions have been combined.  Where the other endpoints request a condition, it should be provided in the format given here.',
       nickname='get'
       )
   def get(self):
-    conditions = []
-    reader = csv.reader(open('Symptom_count_by_condition.csv'))
-    for row in reader:
-        conditions.append(row[0])
+    client = create_client()
+    conditions = getConditionList(client)
 
     return str(conditions), 200
 
-class Symptoms(Resource):
+class ConditionTrackableCounts(Resource):
   @swagger.operation(
-      notes='Get the number of users that that suffer from a condition and each symptom',
+      notes='Get the number of users that that report both a condition, and trackables of a certain type.  For example if you request Symptom/Fibromyalgia, you will be returned a list of how many people with Fibromyalgia also reported each symptom',
       nickname='get',
       parameters=[
           {
@@ -61,43 +68,97 @@ class Symptoms(Resource):
             "allowMultiple": False,
             "dataType": 'string',
             "paramType": "path"
-          }
-      ])
-  def get(self, condition):
-    condition_dict = getSymptomsByCondition(condition)
-    if len(condition_dict.values()) == 0:
-        return "condition not found", 404
-    return json.dumps(condition_dict), 200
-
-class NormalSymptoms(Resource):
-  @swagger.operation(
-      notes='Get the percentage of a users that report a symptom that also report the supplied condition.  Used to estimate how likely it is that a symptom is being cause by a condition',
-      nickname='get',
-      parameters=[
+          },
           {
-              "name": "condition",
-              "description": "The name of the condition",
+              "name": "trackable_type",
+              "description": "The trackable type that the condition will be compared to (Condition, Symptom, Treatment, Tag)",
               "required": True,
               "allowMultiple": False,
               "dataType": 'string',
               "paramType": "path"
           }
       ])
-  def get(self, condition):
-      totals_dict = getSymptomTotals()
-      condition_dict = getSymptomsByCondition(condition)
-      for symptom, count in condition_dict.iteritems():
-          condition_dict[symptom] = float(count) / float(totals_dict[symptom])
-      if len(condition_dict.values()) == 0:
-          return "condition not found", 404
-      return json.dumps(condition_dict), 200
+  def get(self, trackable_type, condition):
+    client = create_client()
+    condition_dict = getCountsByCondition(client, trackable_type, condition)
+    condition_dict.pop('condition', None)
+    condition_dict.pop('id', None)
+    if len(condition_dict.values()) == 0:
+        return "condition not found", 404
+    return json.dumps(condition_dict), 200
+
+class ConditionTrackableCountsNormalized(Resource):
+  @swagger.operation(
+      notes='Get the percent of users that report a trackable and also reported a condition.  Used to estimate if a trackable is likely caused by a condition.  For example if you request Symptom/Fibromyalgia, you will be returned a list of what percentage of people reporting each symptom also reported Fibromyalgia.',
+      nickname='get',
+      parameters=[
+          {
+            "name": "condition",
+            "description": "The name of the condition",
+            "required": True,
+            "allowMultiple": False,
+            "dataType": 'string',
+            "paramType": "path"
+          },
+          {
+              "name": "trackable_type",
+              "description": "The trackable type that the condition will be compared to (Condition, Symptom, Treatment, Tag)",
+              "required": True,
+              "allowMultiple": False,
+              "dataType": 'string',
+              "paramType": "path"
+          }
+      ])
+  def get(self, trackable_type, condition):
+    client = create_client()
+    condition_dict = getCountsByCondition(client, trackable_type, condition)
+    totals_dict = getAllTotals(client,trackable_type)
+
+    condition_dict.pop('condition',None)
+    condition_dict.pop('id', None)
+
+    for key,value in condition_dict.iteritems():
+        condition_dict[key] = float(value) / float(totals_dict[key])
+
+    if len(condition_dict.values()) == 0:
+        return "condition not found", 404
+    return json.dumps(condition_dict), 200
+
+class Counts(Resource):
+  @swagger.operation(
+      notes='Get the total number of users reporting a trackable',
+      nickname='get',
+      parameters=[
+          {
+              "name": "trackable_type",
+              "description": "The type of the trackable, (Symptom, Condition, Treatment, Tag)",
+              "required": True,
+              "allowMultiple": False,
+              "dataType": 'string',
+              "paramType": "path"
+          },
+          {
+              "name": "trackable_name",
+              "description": "The name of the trackable",
+              "required": True,
+              "allowMultiple": False,
+              "dataType": 'string',
+              "paramType": "path"
+          }
+      ])
+  def get(self, trackable_type, trackable_name):
+      client = create_client()
+
+      total = getTotal(client, trackable_type, trackable_name)
+      return total, 200
 
 ##
 ## Actually setup the Api resource routing here
 ##
 api.add_resource(ConditionsList,'/conditions')
-api.add_resource(Symptoms, '/symptoms/<string:condition>')
-api.add_resource(NormalSymptoms, '/symptoms_normal/<string:condition>')
+api.add_resource(ConditionTrackableCounts, '/condition_counts/<string:trackable_type>/<string:condition>')
+api.add_resource(ConditionTrackableCountsNormalized, '/condition_counts_norm/<string:trackable_type>/<string:condition>')
+api.add_resource(Counts, '/counts/<string:trackable_type>/<string:trackable_name>')
 
 
 @app.route('/docs')
--- app-engine-service/find_stats.py
+++ app-engine-service/find_stats.py
@@ -1,29 +1,53 @@
 # It would use a lot of resources to do these calculations every time somebody hits the API
 # So generating figures for every condition and then loading onto google cloud store
 
+#need to authenticate before running by setting an environment variable to your keyfile like:
+#export GOOGLE_APPLICATION_CREDENTIALS=Flaredown-24ac7b1e7653.json
+
+#if updating, be sure to delete old records first or you will get duplicates
+
 import numpy as np
 import pandas as pd
 import random
 from google.cloud import datastore
+import json
 
-def create_client(project_id):
+def create_client():
+    project_id = 'flaredown-149515'
     return datastore.Client(project_id)
 
-def add_trackable(client, trackable_type):
-    key = client.key('Condition-'+trackable_type)
+def add_trackable(client, trackable_type, dict):
+    key = client.key('Condition'+trackable_type)
+    trackable_entity = datastore.Entity(
+        key)
 
-    task = datastore.Entity(
-        key, exclude_from_indexes=['description'])
+    trackable_entity.update(dict)
+    client.put(trackable_entity)
+    return trackable_entity.key
 
-    task.update({
-        'condition': datetime.datetime.utcnow(),
-        'description': description,
-        'done': False
+def add_condition_list(client, conditions):
+    key = client.key('ConditionList')
+    for condition in conditions:
+        trackable_entity = datastore.Entity(
+        key)
+
+        trackable_entity.update({
+            "name": condition,
+        })
+        client.put(trackable_entity)
+    return trackable_entity.key
+
+def add_trackable_count(client, trackable_type, trackable_name, count):
+    key = client.key(trackable_type+"Count")
+    trackable_entity = datastore.Entity(
+        key)
+
+    trackable_entity.update({
+        "name" : trackable_name,
+        "count" : count
     })
-
-    client.put(task)
-
-    return task.key
+    client.put(trackable_entity)
+    return trackable_entity.key
 
 df = pd.read_csv("flaredown_trackable_data_080316.csv")
 df['checkin_date'] = pd.to_datetime(df['checkin_date'])
@@ -32,31 +56,29 @@ anno_df = pd.read_csv("conditions_list.csv")
 conditions_set = set(anno_df['New Name'])
 conditions_set = set(filter(lambda x: x == x, conditions_set)) #remove nan
 
-def writeCSV(trackable_type):
-    outfile = open(trackable_type + '_count_by_condition.csv', 'w')
-    outfile.write('Condition,')
+def writeConditionCounts(client, trackable_type):
     trackables = set(df[df['trackable_type'] == trackable_type]['trackable_name'])
     trackables = list(set(filter(lambda x: x == x, trackables)))  # remove nan
 
-    for trackable in trackables:
-        trackable_clean = ''.join(trackable.split("\n"))
-        trackable_clean = ''.join(trackable_clean.split(","))
-        outfile.write(trackable_clean + ',')
-    outfile.write("\n")
     for condition in conditions_set:
-        print condition
+        print trackable_type + ' ' + condition
+        trackable_dict = {}
+        trackable_dict['condition'] = condition
+        trackable_dict['id'] = condition
         names = anno_df[anno_df['New Name'] == condition]['Condition'].values
 
         #condition_df = df[df['trackable_name'].isin(names)]
         condition_df = df.groupby(['user_id']).filter(lambda x: len(set(x['trackable_name'].values) & set(names)) > 0)
-        outfile.write(condition + ',')
         for trackable in trackables:
             trackable_df = condition_df[condition_df['trackable_name'] == trackable]
-            outfile.write(str(len(trackable_df.groupby('user_id'))) + ",")
-        outfile.write("\n")
+            trackable_clean = ''.join(trackable.split("\n"))
+            trackable_clean = ''.join(trackable_clean.split(","))
+            trackable_dict[trackable_clean] = len(trackable_df.groupby('user_id'))
+        key = add_trackable(client,trackable_type,trackable_dict)
+        print key
 
-def writeCounts(trackable_type):
-    outfile = open(trackable_type + '_counts.csv', 'w')
+
+def writeCounts(client, trackable_type):
     trackables = set(df[df['trackable_type'] == trackable_type]['trackable_name'])
     trackables = list(set(filter(lambda x: x == x, trackables)))  # remove nan
     for trackable in trackables:
@@ -64,14 +86,17 @@ def writeCounts(trackable_type):
         trackable_clean = ''.join(trackable_clean.split(","))
         trackable_df = df[df['trackable_name'] == trackable].groupby('user_id')
         num_of_users = len(trackable_df)
-        outfile.write(trackable_clean + ',' + str(num_of_users) + "\n")
+        add_trackable_count(client, trackable_type, trackable_clean, num_of_users)
 
-writeCSV('Symptom')
-writeCSV('Condition')
-writeCSV('Treatment')
-writeCSV('Tag')
+client = create_client()
+#writeConditionCounts(client, 'Symptom')
+#writeConditionCounts(client,'Condition')
+#writeConditionCounts(client,'Treatment')
+writeConditionCounts(client,'Tag')
 
-#writeCounts('Symptom')
-#writeCounts('Condition')
-#writeCounts('Treatment')
-#writeCounts('Tag')
+#add_condition_list(client,conditions_set)
+
+#writeCounts(client,'Symptom')
+#writeCounts(client,'Condition')
+#writeCounts(client,'Treatment')
+#writeCounts(client,'Tag')
